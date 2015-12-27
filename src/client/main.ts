21a72336507f826dfbad917d44e8f5a8277690f6
lzonjic <lino.zonjic@gmail.com>
2015-12-27 03:22:53 +0100

    beginning

---
+++ src/client/main.ts
@@ -0,0 +1,15 @@
+import 'bootstrap';
+import {Aurelia} from 'aurelia-framework';
+
+export function configure(aurelia: Aurelia) {
+  aurelia.use
+    .standardConfiguration()
+    .developmentLogging();
+
+  aurelia.use.plugin('aurelia-animator-css');
+
+  //Anyone wanting to use HTMLImports to load views, will need to install the following plugin.
+  //aurelia.use.plugin('aurelia-html-import-template-loader')
+
+  aurelia.start().then(a => a.setRoot());
+}
---
+++ src/server/server.ts
@@ -0,0 +1,28 @@
+import * as express from 'express';
+import * as path from 'path';
+import * as browserSync from 'browser-sync';
+
+var app = express();
+
+//app.set('view engine', 'html'); // so you can render('index')
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/api/nav', (req, res) => {
+    res.send({
+        data: [
+            'Home',
+            'About'
+        ]
+    });
+});
+
+var port: number = process.env.PORT || 3000;
+var server = app.listen(port, () => {
+    var listeningPort: number = server.address().port;
+    browserSync({
+      proxy: 'localhost:' + port,
+      files: [__dirname + '/public/**/*.{js,css,html}']
+    });
+    console.log(`Yo!! The server is listening on port: ${listeningPort}`);
+});
